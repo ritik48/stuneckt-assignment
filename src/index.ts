2e06dd4ce9fa8924cb159394f5860940f408b86c
Ritik Raj <raj769417@gmail.com>
2024-05-12 03:02:34 +0530

    fix: added mongodb url from env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+
+dotenv.config();
 import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
